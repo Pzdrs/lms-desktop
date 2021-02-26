f140b6fa3ed3c75c7245ef46a1a11c70e9717aff
Pycrs <petrbohac3@seznam.cz>
2021-02-26 23:44:38 +0100

    Project setup

---
+++ src/main/java/pb/lms_desktop/Main.java
@@ -0,0 +1,15 @@
+package pb.lms_desktop;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
