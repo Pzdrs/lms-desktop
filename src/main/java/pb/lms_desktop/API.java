bb2aa981bb59c4e6d3e9be88bca6814f0003f8ea
Pycrs <petrbohac3@seznam.cz>
2021-03-20 01:14:24 +0100

    Fixed the encoding

--- src/main/java/pb/lms_desktop/API.java
+++ src/main/java/pb/lms_desktop/API.java
@@ -1,11 +1,21 @@
 package pb.lms_desktop;
 
+import javafx.util.Pair;
+import jdk.internal.util.xml.impl.Input;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.*;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class API {
     private String baseURL;
@@ -20,8 +30,10 @@ public class API {
         return client.execute(new HttpGet(baseURL + s));
     }
 
-    public HttpResponse post(String s) throws IOException {
-        return client.execute(new HttpPost(baseURL + s));
+    public HttpResponse post(String s, BasicNameValuePair... data) throws IOException {
+        HttpPost req = new HttpPost(baseURL + s);
+        req.setEntity(new UrlEncodedFormEntity(new ArrayList<>(Arrays.asList(data)), StandardCharsets.UTF_8));
+        return client.execute(req);
     }
 
     public HttpResponse delete(String s) throws IOException {
@@ -36,17 +48,34 @@ public class API {
         return client.execute(new HttpPatch(baseURL + s));
     }
 
-    public static String toReadable(HttpResponse response) {
-        String s = "";
+    public Pair<Integer, String> login(String username, String password) {
         try {
-            int data = response.getEntity().getContent().read();
+            HttpResponse response = post("/auth/login",
+                    new BasicNameValuePair("username", username),
+                    new BasicNameValuePair("password", password));
+            return new Pair<>(response.getStatusLine().getStatusCode(), asText(response.getEntity().getContent()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Pair<>(0, "");
+    }
+
+    public static String asText(HttpResponse response) throws IOException {
+        return asText(response.getEntity().getContent());
+    }
+
+    public static String asText(InputStream stream) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
+        StringBuilder s = new StringBuilder();
+        try {
+            int data = reader.read();
             while (data != -1) {
-                s += (char) data;
-                data = response.getEntity().getContent().read();
+                s.append((char) data);
+                data = reader.read();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return s;
+        return s.toString();
     }
 }
