10fa7b77accab1e760ed3b234caab489364f70dd
Pycrs <petrbohac3@seznam.cz>
2021-03-22 14:23:08 +0100

    Moved login logic and and page size initiation here

--- src/main/java/pb/lms_desktop/Utils.java
+++ src/main/java/pb/lms_desktop/Utils.java
@@ -2,9 +2,12 @@ package pb.lms_desktop;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.util.Pair;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import pb.lms_desktop.dialogs.LoginDialog;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class Utils {
     public static Parent loadFXML(String fxml) throws IOException {
@@ -13,5 +16,17 @@ public class Utils {
 
     public static boolean checkPassword(String password) {
         return BCrypt.checkpw(password, Main.getStore().getUser().getPassword());
+    }
+
+    public static Pair<Boolean, Pair<String, String>> promptLogin() {
+        Optional<Pair<Boolean, Pair<String, String>>> result = new LoginDialog().showAndWait();
+        if (result.isPresent() && result.get().getKey())
+            return new Pair<>(true, new Pair<>(result.get().getValue().getKey(), result.get().getValue().getValue()));
+        return new Pair<>(false, null);
+    }
+
+    public static void initPageSize(double width) {
+        Main.stage.setWidth(width);
+        Main.stage.centerOnScreen();
     }
 }
