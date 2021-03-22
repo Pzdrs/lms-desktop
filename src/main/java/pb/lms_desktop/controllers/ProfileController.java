094bbc02632497aa4d4d1158a5fa2d366eeb9aa1
Pycrs <petrbohac3@seznam.cz>
2021-03-22 16:58:28 +0100

    Password change working now

--- src/main/java/pb/lms_desktop/controllers/ProfileController.java
+++ src/main/java/pb/lms_desktop/controllers/ProfileController.java
@@ -6,6 +6,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.util.Pair;
+import org.apache.http.HttpResponse;
+import org.json.JSONObject;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import pb.lms_desktop.API;
 import pb.lms_desktop.Main;
 import pb.lms_desktop.Utils;
 import pb.lms_desktop.dialogs.ChangeFirstNameDialog;
@@ -13,6 +17,7 @@ import pb.lms_desktop.dialogs.ChangeLastNameDialog;
 import pb.lms_desktop.dialogs.PasswordConfirmDialog;
 import pb.lms_desktop.store.modules.User;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -47,9 +52,8 @@ public class ProfileController implements Initializable {
 
         // Making sure change pass button is disabled while old pass is incorrect and new passwords dont match
         changePassword.disableProperty().bind(Bindings.createBooleanBinding(
-                () -> !Main.getStore().getUser().getPassword().equals(oldPassword.getText())
-                        && newPassword.getText().equals(newPasswordConfirm.getText()),
-                oldPassword.textProperty(),
+                () -> (newPassword.getText().isEmpty() || newPasswordConfirm.getText().isEmpty()) ||
+                        !newPassword.getText().equals(newPasswordConfirm.getText()),
                 newPassword.textProperty(),
                 newPasswordConfirm.textProperty()));
     }
@@ -58,20 +62,64 @@ public class ProfileController implements Initializable {
         Optional<String> firstNameDialog = new ChangeFirstNameDialog().showAndWait();
         if (!firstNameDialog.isPresent()) return;
         Pair<Integer, String> result = Main.getApi().changeUserProperty("firstName", firstNameDialog.get());
-        if (result.getKey() == 200)
-            Main.getStore().getUser().setFirstName(firstNameDialog.get());
+        if (result.getKey() != 200) {
+            Alert error = new Alert(Alert.AlertType.ERROR);
+            error.setTitle("Error");
+            error.setHeaderText("Couldn't change your first name, please try again later.");
+            error.show();
+            return;
+        }
+        Main.getStore().getUser().setFirstName(firstNameDialog.get());
     }
 
     public void changeLastName() {
         Optional<String> lastNameDialog = new ChangeLastNameDialog().showAndWait();
         if (!lastNameDialog.isPresent()) return;
         Pair<Integer, String> result = Main.getApi().changeUserProperty("lastName", lastNameDialog.get());
-        if (result.getKey() == 200)
-            Main.getStore().getUser().setLastName(lastNameDialog.get());
+        if (result.getKey() != 200) {
+            Alert error = new Alert(Alert.AlertType.ERROR);
+            error.setTitle("Error");
+            error.setHeaderText("Couldn't change your last name, please try again later.");
+            error.show();
+            return;
+        }
+        Main.getStore().getUser().setLastName(lastNameDialog.get());
     }
 
     public void changePassword() {
-        // TODO: 3/21/2021 get bcrypt in here
+        if (!BCrypt.checkpw(oldPassword.getText(), Main.getStore().getUser().getPassword())) {
+            Alert error = new Alert(Alert.AlertType.ERROR);
+            error.setTitle("Error");
+            error.setHeaderText("That's not the correct password, try again.");
+            error.show();
+            return;
+        }
+        Pair<Integer, String> result = Main.getApi().changeUserProperty("password", newPassword.getText());
+        if (result.getKey() != 200) {
+            Alert error = new Alert(Alert.AlertType.ERROR);
+            error.setTitle("Error");
+            error.setHeaderText("Couldn't change your password, please try again later.");
+            error.show();
+            return;
+        }
+        try {
+            HttpResponse userReloadResult = Main.getApi().get("/users/" + Main.getStore().getUser().getId());
+            JSONObject user = new JSONObject(API.asText(userReloadResult.getEntity().getContent())).getJSONArray("user").getJSONObject(0);
+
+            Main.getStore().getUser().setPassword(user.getString("password"));
+
+            // Reset password fields
+            oldPassword.setText("");
+            newPassword.setText("");
+            newPasswordConfirm.setText("");
+
+            Alert error = new Alert(Alert.AlertType.INFORMATION);
+            error.setTitle("Password changed");
+            error.setHeaderText("Your password has been changed.");
+            error.show();
+        } catch (IOException e) {
+            System.out.println("Couldn't reload user");
+        }
     }
 
     public void logout() {
