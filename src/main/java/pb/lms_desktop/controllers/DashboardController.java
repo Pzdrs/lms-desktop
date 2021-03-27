1f51f1bf3db39d76dd8c7db26584070706b33476
Pycrs <petrbohac3@seznam.cz>
2021-03-27 16:25:19 +0100

    Dashboard - added latest borrows

--- src/main/java/pb/lms_desktop/controllers/DashboardController.java
+++ src/main/java/pb/lms_desktop/controllers/DashboardController.java
@@ -1,21 +1,26 @@
 package pb.lms_desktop.controllers;
 
+import javafx.beans.binding.Bindings;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import org.apache.http.HttpResponse;
 import org.json.JSONObject;
 import pb.lms_desktop.API;
 import pb.lms_desktop.Main;
 import pb.lms_desktop.Utils;
+import pb.lms_desktop.store.modules.History;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
-    public VBox container;
-    public Label booksTotal, booksInUse, users;
+    public VBox container, latestBorrows, myBorrows;
+    public Label booksTotal, booksInUse, users, user;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -37,6 +42,29 @@ public class DashboardController implements Initializable {
             users.setText(String.valueOf(statisticsObject.getInt("users")));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        // Bind first name to the welcome message
+        user.textProperty().bind(Bindings.createStringBinding(() -> Main.getStore().getUser().getFirstName() + ".", Main.getStore().getUser().firstNameProperty()));
+
+        int i = 0;
+        for (History history : Main.getStore().getHistory()) {
+            if (i == 5) break;
+            if (history.getUser().getId().equals(Main.getStore().getUser().getId()))
+                myBorrows.getChildren().add(new Label(history.getUser().getFullName()));
+
+            Label whenWho = new Label(Utils.timeDiff(history.getFrom()) + " ago by " + history.getUser().getFirstName());
+            Label book = new Label(history.getBook().getTitle());
+
+            book.setStyle("-fx-font-weight: bold");
+            whenWho.setStyle("-fx-text-fill: green");
+
+            VBox latest = new VBox(book, new Label("by " + history.getBook().getAuthor().getFullName()), whenWho);
+            VBox.setMargin(latest, new Insets(15, 0, 0, 0));
+
+            latest.setAlignment(Pos.TOP_CENTER);
+            latestBorrows.getChildren().add(latest);
+            i++;
         }
     }
 }
