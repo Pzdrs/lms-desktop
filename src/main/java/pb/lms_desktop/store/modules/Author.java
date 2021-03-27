cbfd451a4cbedf2dde5ea098e96391fba68f5324
Pycrs <petrbohac3@seznam.cz>
2021-03-27 15:49:46 +0100

    Moved to observable properties

--- src/main/java/pb/lms_desktop/store/modules/Author.java
+++ src/main/java/pb/lms_desktop/store/modules/Author.java
@@ -1,33 +1,55 @@
 package pb.lms_desktop.store.modules;
 
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.beans.value.ObservableValue;
 
 import java.util.Date;
 
 public class Author {
-    private String id, firstName, lastName;
+    private StringProperty id, firstName, lastName, fullName;
     private Date born, died, createdAt;
 
     public Author(String id, String firstName, String lastName, Date born, Date died, Date createdAt) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.id = new SimpleStringProperty(id);
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+        this.fullName = new SimpleStringProperty(firstName + " " + lastName);
         this.born = born;
         this.died = died;
         this.createdAt = createdAt;
     }
 
     public String getId() {
+        return id.get();
+    }
+
+    public StringProperty idProperty() {
         return id;
     }
 
     public String getFirstName() {
+        return firstName.get();
+    }
+
+    public StringProperty firstNameProperty() {
         return firstName;
     }
 
     public String getLastName() {
+        return lastName.get();
+    }
+
+    public StringProperty lastNameProperty() {
         return lastName;
+    }
+
+    public String getFullName() {
+        return fullName.get();
+    }
+
+    public StringProperty fullNameProperty() {
+        return fullName;
     }
 
     public Date getBorn() {
@@ -52,9 +74,5 @@ public class Author {
                 ", died=" + died +
                 ", createdAt=" + createdAt +
                 '}';
-    }
-
-    public ObservableValue<String> getFullName() {
-        return new SimpleStringProperty(this.firstName + " " + this.lastName);
     }
 }
