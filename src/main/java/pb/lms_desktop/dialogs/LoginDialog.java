e35cfdf757fbd36fe81d5a19075688f4b4fc7371
Pycrs <petrbohac3@seznam.cz>
2021-03-31 15:47:06 +0200

    Ready for the day D, i think

--- src/main/java/pb/lms_desktop/dialogs/LoginDialog.java
+++ src/main/java/pb/lms_desktop/dialogs/LoginDialog.java
@@ -1,28 +1,14 @@
 package pb.lms_desktop.dialogs;
 
-import com.jfoenix.controls.JFXDialog;
-import com.jfoenix.controls.JFXDialogLayout;
-import com.jfoenix.controls.JFXPasswordField;
-import com.jfoenix.controls.JFXTextField;
 import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.*;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.util.Pair;
 import org.json.JSONObject;
 import pb.lms_desktop.Main;
 import pb.lms_desktop.Utils;
 import pb.lms_desktop.store.modules.User;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class LoginDialog extends Dialog<Pair<Boolean, Pair<String, String>>> {
     public TextField username;
